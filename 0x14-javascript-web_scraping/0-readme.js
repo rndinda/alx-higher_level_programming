b4e0b4af908388111e16d82c04afbf3adf3ebf10
Ndinda rose <rndinda73@gmail.com>
2023-04-26 11:40:35 +0300

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@
 const file = process.argv[0];
 const fs = require('fs');
 
-fs.readFile(file, 'utf8', (error, data) => {
+fs.readFile(filePath, 'utf8', (error, data) => {
   if (error) {
     console.log(error);
   } else {
