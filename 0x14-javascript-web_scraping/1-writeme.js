1d1f6f1472fd7b5dc48693b0997716c484a84ac8
Ndinda rose <rndinda73@gmail.com>
2023-04-29 08:46:51 +0300

    Write file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
+
+fs.writeFile('./text_file/filetwo.txt', 'Oyaa,andika hii','utf-8',(error)=>{
+  if(error){
+    console.log(error);
+  }
+
 });
