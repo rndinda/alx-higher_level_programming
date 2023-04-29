883f429634a5e5c698daa1921dc2aff9b71f0682
Ndinda rose <rndinda73@gmail.com>
2023-04-29 08:57:17 +0300

    Task Correction

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-fs.writeFile('./text_file/my_file.txt', 'Python is cool','utf-8',(error)=>{
+fs.writeFile('./my_file.txt', 'Python is cool','utf-8',(error)=>{
   if(error){
     console.log(error);
   }
