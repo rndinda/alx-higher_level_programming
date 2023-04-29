3cd6acd10faea76c465409c8d7476a554f659d1d
Ndinda rose <rndinda73@gmail.com>
2023-04-29 08:31:29 +0300

    Read and print file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 // script that reads and prints the content of a given file
 
-const file = process.argv[0];
 const fs = require('fs');
 
-fs.readFile(filePath, 'utf8', (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
+fs.readFile('./text_file/fileone.txt','utf-8',(error,data)=>{
+  if(error){
+    console.error(error);
   }
-});
+  console.log(data)
+})
